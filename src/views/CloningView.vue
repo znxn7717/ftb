17a25cf0de8a0bac1e00d818639ddd327c3e5671
znxn7717 <znxn7717@gmail.com>
2026-06-09 09:12:19 +0000

    0

---
+++ src/views/CloningView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UCard class="mx-auto mt-3 p-4 max-w-4xl">
+    <template #header><span class="text-2xl font-bold">Settings</span></template>
+    <div class="flex flex-col gap-4 text-start dark:text-neutral-300">
+      <div class="border border-neutral-400 rounded-sm p-4 space-y-4">
+
+      </div>
+    </div>
+  </UCard>
+</template>
